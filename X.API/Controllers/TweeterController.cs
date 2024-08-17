a79493f5d9a8d7ff88f97daae8b77a820a0f5799
phucngo2 <phucnnh21@gmail.com>
2024-08-17 12:23:23 +0700

    Feature: Tweeter controller

---
+++ X.API/Controllers/TweeterController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using X.Application.Services.TwitterServices;
+using X.Application.Services.TwitterServices.Dtos;
+
+namespace X.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TweeterController(ITweeterService tweeterService) : ControllerBase
+{
+    private readonly ITweeterService _tweeterService = tweeterService;
+
+    [HttpPost]
+    public async Task<IActionResult> ListTweetMediasAsync(TweetMediasRequest request)
+    {
+        var res = await _tweeterService.ListTweetMediasAsync(request);
+        return Ok(res);
+    }
+}
