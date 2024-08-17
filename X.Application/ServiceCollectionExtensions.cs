1ff337b7596f1f25adcd2ee45511b70651810053
phucngo2 <phucnnh21@gmail.com>
2024-08-17 11:13:34 +0700

    Config: AutoMapper

---
+++ X.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace X.Application;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
