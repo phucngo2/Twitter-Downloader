86ca1ab5024960907933577d3ec20eb56f24cbcf
phucngo2 <phucnnh21@gmail.com>
2024-08-18 15:41:33 +0700

    Feature: [webapp] Error message

--- X.WebApp/src/lib/components/Container.svelte
+++ X.WebApp/src/lib/components/Container.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import SearchForm from "./SearchForm.svelte";
   import { useTweetMediaMutation } from "../queries";
+  import ErrorAlert from "./ErrorAlert.svelte";
 
   const mutation = useTweetMediaMutation();
   function handleMutation(event: CustomEvent<string>) {
@@ -8,6 +9,12 @@
   }
 </script>
 
-<div>
+<div class="flex flex-col gap-4">
   <SearchForm on:submit={handleMutation} isPending={$mutation.isPending} />
+
+  {#if $mutation.isError}
+    <ErrorAlert message={$mutation.error.message} />
+  {:else if $mutation.data}
+    <div>{$mutation.data}</div>
+  {/if}
 </div>
