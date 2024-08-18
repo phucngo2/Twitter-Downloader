50e2f91fc44f140cf04a81d1de8285170358abfa
phucngo2 <phucnnh21@gmail.com>
2024-08-18 15:12:32 +0700

    Feature: [webapp] Axios + Svelte Query error handling

--- X.WebApp/src/lib/config/axios.config.ts
+++ X.WebApp/src/lib/config/axios.config.ts
@@ -4,4 +4,8 @@ const axios = axiosLib.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
+axios.interceptors.response.use(undefined, (error) => {
+  throw new Error(error?.response?.data?.message);
+});
+
 export { axios };
