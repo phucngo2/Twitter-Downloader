1d6d3988ba8feb6ffa3d398734aa698c2a225e1e
phucngo2 <phucnnh21@gmail.com>
2024-08-18 15:46:46 +0700

    Feature: [webapp] Tweet types

--- X.WebApp/src/lib/queries/tweet-media.mutation.ts
+++ X.WebApp/src/lib/queries/tweet-media.mutation.ts
@@ -1,11 +1,12 @@
 import { createMutation } from "@tanstack/svelte-query";
 import { axios, TWITTER_API_URL } from "../config";
+import type { TweetMediaResponse } from "../types";
 
 export const useTweetMediaMutation = () => {
   return createMutation({
     mutationKey: ["tweet-medias"],
     mutationFn: (url: string) => {
-      return axios.post(TWITTER_API_URL, {
+      return axios.post<TweetMediaResponse>(TWITTER_API_URL, {
         url,
       });
     },
---
+++ X.WebApp/src/lib/types/tweet.types.ts
@@ -0,0 +1,18 @@
+export interface TweetVideoVariant {
+  bitrate?: number;
+  contentType: "video/mp4" | "string";
+  url: "string";
+}
+
+export interface TweetMedia {
+  displayUrl: string;
+  expendedUrl: string;
+  idStr: string;
+  mediaUrlHttps: string;
+  type: "video" | "photo" | "animated_gif";
+  videoVariants: TweetVideoVariant[];
+}
+
+export interface TweetMediaResponse {
+  medias: TweetMedia[];
+}
